89ee6e0925b4e8e9fef28f30e0f0eecfb9a3f097
Kenta Suzuki <suzuken326@gmail.com>
2015-12-02 22:13:49 +0900

    complex caliculation with given x

--- eval/eval_test.go
+++ eval/eval_test.go
@@ -14,11 +14,9 @@ func TestEval(t *testing.T) {
 		types.Number(1),
 	}
 
-	r, err := Eval(exp, env)
-	if err != nil {
+	if _, err := Eval(exp, env); err != nil {
 		t.Fatalf("eval but error : %s", err)
 	}
-	t.Logf("%v\n", r)
 }
 
 func TestEvalSum(t *testing.T) {
@@ -39,4 +37,42 @@ func TestEvalSum(t *testing.T) {
 		t.Fatalf("1 + 2 should 3 but not: %v", r)
 	}
 
+}
+
+func TestEvalDefineAndCaliculation(t *testing.T) {
+	env := NewEnv()
+	env.Setup()
+
+	if _, err := Eval([]types.Expression{
+		types.Symbol("define"),
+		types.Symbol("x"),
+		types.Number(1),
+	}, env); err != nil {
+		t.Fatalf("eval but error : %s", err)
+	}
+
+	// (+ x 3 (* 5 (- 5 2)))
+	exp := []types.Expression{
+		types.Symbol("+"),
+		types.Symbol("x"),
+		types.Number(3),
+		[]types.Expression{
+			types.Symbol("*"),
+			types.Number(5),
+			[]types.Expression{
+				types.Symbol("-"),
+				types.Number(5),
+				types.Number(2),
+			},
+		},
+	}
+
+	r, err := Eval(exp, env)
+	if err != nil {
+		t.Fatalf("eval but error : %s", err)
+	}
+
+	if r != types.Number(19) {
+		t.Fatalf("given x = 1, (+ x 3 (* 5 (- 5 2))) should equal 19 but get: %v", r)
+	}
 }
