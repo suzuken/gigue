4e4d178433cbac564dc93c123d3991614818dd4a
Kenta Suzuki <suzuken326@gmail.com>
2015-12-15 20:38:01 +0900

    test main

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 	env.Setup()
 
 	flag.Parse()
-	if len(flag.Args()) == 0 {
+	if flag.NArg() == 0 {
 		reader := bufio.NewReader(os.Stdin)
 		for {
 			fmt.Print("> ")
@@ -34,28 +34,31 @@ func main() {
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "eval error: %s\n", err)
 			} else {
-				showEval(exp)
+				output(exp)
 			}
 		}
 	} else {
-		filename := flag.Arg(1)
-		f, err := os.Open(filename)
+		exp, err := evalFile(flag.Arg(0), env)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "cannot open file: %s\n", err)
+			fmt.Fprintf(os.Stderr, "%s\n", err)
 			os.Exit(1)
 		}
-		defer f.Close()
-		exp, err := ev(f, env)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "eval error: %s\n", err)
-			os.Exit(1)
-		}
-		showEval(exp)
+		output(exp)
 	}
 }
 
-func showEval(exp types.Expression) {
+// output simply output results.
+func output(exp types.Expression) {
 	fmt.Printf("%v\n", exp)
+}
+
+func evalFile(filename string, env *eval.Env) (types.Expression, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	return ev(f, env)
 }
 
 func ev(r io.Reader, env *eval.Env) (types.Expression, error) {
--- main_test.go
+++ main_test.go
@@ -1,8 +1,20 @@
 package main
 
 import (
+	"github.com/suzuken/gs/eval"
+	"strings"
 	"testing"
 )
 
 func TestEv(t *testing.T) {
+	env := eval.NewEnv()
+	env.Setup()
+
+	r := strings.NewReader(`
+(define x 1)
+(print x)
+	`)
+	if _, err := ev(r, env); err != nil {
+		t.Fatalf("eval error read from io.Reader: %s", err)
+	}
 }
